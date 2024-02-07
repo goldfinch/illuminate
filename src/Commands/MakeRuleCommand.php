255c0852c18d1ac91401c08f7459be756a31a562
Art Karasev <2291826+goldfinch@users.noreply.github.com>
2024-02-07 14:26:32 +1300

    WIP

--- src/Commands/MakeRuleCommand.php
+++ src/Commands/MakeRuleCommand.php
@@ -3,7 +3,6 @@
 namespace Goldfinch\Illuminate\Commands;
 
 use Goldfinch\Taz\Console\GeneratorCommand;
-use Symfony\Component\Console\Command\Command;
 
 #[AsCommand(name: 'make:rule')]
 class MakeRuleCommand extends GeneratorCommand
@@ -19,11 +18,4 @@ class MakeRuleCommand extends GeneratorCommand
     protected $stub = './stubs/rule.stub';
 
     protected $prefix = 'Rule';
-
-    protected function execute($input, $output): int
-    {
-        parent::execute($input, $output);
-
-        return Command::SUCCESS;
-    }
 }
